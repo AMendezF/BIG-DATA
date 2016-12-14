5b87010160adf69a32b0323c48c9946b56e0ebb6
AMendezF <a.mendezworks@gmail.com>
2016-12-14 17:58:05 +0100

    Add files via upload

--- Proyecto11/proyecto11.py
+++ Proyecto11/proyecto11.py
@@ -1,5 +1,3 @@
-import re
-
 __author__ = 'Alejandro Mendez Fernandez bm0058'
 
 
@@ -57,6 +55,84 @@ class IOManager:
         self.get_f_out().close()
 
 
+class Station:
+    # TODO comment all
+
+    def __init__(self):
+        self.__data = {}
+
+    def __variance(self, values):
+        """
+        Calcula la varianza.
+        :return float:
+        """
+        number_intervals = len(values)
+
+        total_times = []
+        for i in range(number_intervals):       # Concatenamos todos los dias
+            for times in values[i]:
+                total_times.append(times)       # para su posterior analisis.
+
+        number_elements = len(total_times)   # Numero de elementos.
+        total_average = self.__average(values)
+
+        numerator = 0
+        for time in total_times:                        # Calculo del
+            numerator += (time - total_average) ** 2    # numerador.
+
+        return round(float(numerator / (number_elements - 1)), 2)      # varianza
+
+    def typical_deviation(self, parameter, month):
+        return self.__variance(parameter, month)
+
+    @staticmethod
+    def __average(values):
+
+        sumatory = 0.0
+        for value in values:
+            sumatory += value
+
+        return sumatory / len(values)
+
+    def average_value(self, parameter, year_month):
+        values = map(float, self.get_data()[parameter][year_month])
+        return self.__average(values)
+
+    def __add_parameter(self, parameter):
+        self.get_data()[parameter] = {}
+
+    def add_data(self, raw_data, parameter):
+        if parameter not in self.__data:
+            self.__add_parameter(parameter)
+
+        date = raw_data[14:18]
+        data = raw_data[18:]
+
+        appear_null_data = data.find('N')
+        while appear_null_data != -1:
+            data = data[:appear_null_data - 5] + data[appear_null_data + 1:]
+            appear_null_data = data.find('N')
+
+        self.__data[parameter][date] = data.split('V')[:-1]
+
+    def get_data(self):
+        return self.__data
+
+    def tostring(self):
+        """
+        Formatea a texto los datos.
+        """
+        text = ""
+
+        for parameter in sorted(self.get_data()):
+            text += "  > Polluting Agent(" + parameter + "):\n"
+            text += "    > Year/Month:\n"
+            for date in sorted(self.get_data()[parameter]):
+                text += "        " + date[:2] + "/" + date[2:] + ":" + str(self.get_data()[parameter][date]) + "\n"
+
+        return text
+
+
 class DataBase:
     """
     Obtiene y almacena datos.
@@ -67,13 +143,11 @@ class DataBase:
         :param manager: managerIO.
         """
         self.__manager = manager
-        self.__data_satations = {}      # TODO comment
+        self.__data = {}      # TODO comment
         self.__stations = ['28079004', '28079038', '28079040']       # TODO comment
         self.__parameters = ['06', '08', '10', '14']
         for station in self.__stations:
-            self.__data_satations[station] = {}
-            for parameter in self.__parameters:
-                self.__data_satations[station][parameter] = {}
+            self.__data[station] = Station()
 
     def fill_data(self):
         """
@@ -83,39 +157,18 @@ class DataBase:
 
         line = raw_in.readline().strip('\n')    # Lectura
         while line != '':           # Mientras queden datos en el fichero:
-            self.add_data_stations(line)     # Apuntamos fecha del intervalo de datos.
+            self.add_data(line)     # TODO comment
             line = raw_in.readline().strip('\n')    # Lectura
 
-            """interval = []
-            while line.find(':') == -1 and line != "":   # Mientras haya datos del intervalo:
-                if line == '999999':    # Para medidas indefinidas
-                    interval += [1500]  # suponemos que son 25 min.
-                else:
-                    interval += [int(line)]     # Dato tipo integer
-                line = raw_in.readline().strip('\n')    # Lectura
-            self.add_times(interval)    # Anhiadimos el intervalo al conjunto de tiempos"""
-
-    def add_data_stations(self, line):   # Anhiadir fecha
+    def add_data(self, line):   # TODO comment
         station = line[:8]
         if station in self.get_stations():
             parameter = line[8:10]
             if parameter in self.get_parameters():
-                date = line[14:18]
-                data = line[18:]
+                self.__data[station].add_data(line, parameter)
 
-                appear_null_data = data.find('N')
-                while appear_null_data != -1:
-                    data = data[:appear_null_data - 5] + data[appear_null_data + 1:]
-                    appear_null_data = data.find('N')
-
-                self.__data_satations[station][parameter][date] = data.split('V')[:-1]
-                print station + " " + parameter + " " + date + " " + str(self.__data_satations[station][parameter][date])
-
-    def get_data_satations(self):
-        return self.__data_satations
-
-    def add_times(self, interval):  # Anhiadir intervalo de medidas
-        self.__stations += [interval]
+    def get_data(self):     # TODO comment
+        return self.__data
 
     def get_stations(self):
         return self.__stations
@@ -123,14 +176,16 @@ class DataBase:
     def get_parameters(self):
         return self.__parameters
 
-    def to_string(self):
+    def tostring(self):
         """
         Formatea a texto los datos.
         """
-        text = ''
-        for i in range(len(self.get_data_satations())):
-            text += "Captura " + self.get_data_satations()[i] + ":\n"
-            text += "->Datos: " + str(self.get_stations()[i]) + "\n"
+        text = ""
+
+        for station in sorted(self.get_data()):
+            text += "\nStation(" + station + "):\n"
+            text += self.get_data()[station].tostring()
+
         return text
 
 
@@ -143,6 +198,8 @@ class Analyze:
         self.__manager = gestor     # Gestor de entrada y salida
         self.__data = DataBase(gestor)                  # Base de datos
         self.__data.fill_data()     # Se importan los datos.
+        print self.__data.tostring()
+        print str(self.__data.get_data()['28079004'].average_value('08', '1601'))
 
         """self.__wait_times = []      # Tiempos de espera
         for time_interval in self.__data.get_stations():
@@ -179,27 +236,6 @@ class Analyze:
             sumatory += time  # Calculo de la media de valores.
 
         return round(float(sumatory / number_elements), 2)
-
-    def __variance(self, wait_times):
-        """
-        Calcula la varianza de los tiempos de espera.
-        :return float:
-        """
-        number_intervals = len(wait_times)
-
-        total_times = []
-        for i in range(number_intervals):       # Concatenamos todos los dias
-            for times in wait_times[i]:
-                total_times.append(times)       # para su posterior analisis.
-
-        number_elements = len(total_times)   # Numero de elementos.
-        total_average = self.__average(wait_times)
-
-        numerator = 0
-        for time in total_times:                        # Calculo del
-            numerator += (time - total_average) ** 2    # numerador.
-
-        return round(float(numerator / number_elements), 2)      # varianza
 
     def analyze(self):
         """
